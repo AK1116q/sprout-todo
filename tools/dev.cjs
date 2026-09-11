892df1f008395dd750d4108e83d2f3bf70e91a32
AK1116q <148340327+AK1116q@users.noreply.github.com>
2026-09-11 11:30:37 +1000

    Add live Electron development launcher

---
+++ tools/dev.cjs
@@ -0,0 +1,6 @@
+const {spawn}=require('node:child_process');
+const electron=require('electron');
+const child=spawn(electron,['.'],{stdio:'inherit',env:{...process.env,SPROUT_DEV:'1'}});
+child.on('exit',(code,signal)=>process.exit(code??(signal?1:0)));
+process.on('SIGINT',()=>child.kill('SIGINT'));
+
