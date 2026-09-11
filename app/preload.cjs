405f98df167caa7171f0b3216b90f3c1554ab382
AK1116q <148340327+AK1116q@users.noreply.github.com>
2026-09-11 11:21:55 +1000

    Add Sprout desktop source app/preload.cjs

---
+++ app/preload.cjs
@@ -0,0 +1,12 @@
+const {contextBridge,ipcRenderer}=require('electron');
+contextBridge.exposeInMainWorld('sprout',Object.freeze({
+ get:()=>ipcRenderer.invoke('sprout:get'),
+ act:(action,payload)=>ipcRenderer.invoke('sprout:act',action,payload),
+ panel:page=>ipcRenderer.invoke('sprout:panel',page),
+ closePanel:()=>ipcRenderer.send('sprout:close-panel'),
+ pointer:interactive=>ipcRenderer.send('sprout:pointer',interactive),
+ menu:()=>ipcRenderer.send('sprout:menu'),
+ onState:callback=>{const listener=(_event,state)=>callback(state);ipcRenderer.on('sprout:state',listener);return()=>ipcRenderer.removeListener('sprout:state',listener);},
+ onPage:callback=>{const listener=(_event,page)=>callback(page);ipcRenderer.on('sprout:page',listener);return()=>ipcRenderer.removeListener('sprout:page',listener);}
+}));
+
